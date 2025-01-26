6d05c08bbc74188bb835c67428e3143ef0f53598
Am Joe <josephyanum75@gmail.com>
2025-01-26 05:32:23 +0000

    fix: add a return keyword to fix the void error

--- client/components/svgs/ClockIcon.tsx
+++ client/components/svgs/ClockIcon.tsx
@@ -1,5 +1,5 @@
 const ClockIcon = ()=> {
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+   return <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <rect width="24" height="24" rx="4" fill="#E8E8E8"/>
 <g clip-path="url(#clip0_111_413)">
 <path d="M12.0001 17.8334C15.2218 17.8334 17.8334 15.2218 17.8334 12.0001C17.8334 8.77833 15.2218 6.16675 12.0001 6.16675C8.77833 6.16675 6.16675 8.77833 6.16675 12.0001C6.16675 15.2218 8.77833 17.8334 12.0001 17.8334Z" stroke="#B7B7B7" stroke-width="1.16667" stroke-linejoin="round"/>
