e1aaff84004b654186b9fd499485fe113b7a3350
Luis Carrión <louiss862@gmail.com>
2025-01-27 20:49:15 -0300

    refactor: reuse part of sections in new component

--- client/components/landing/home/offer.tsx
+++ client/components/landing/home/offer.tsx
@@ -11,19 +11,16 @@ import {
   IntegrationIcon,
   NotificationIcon,
 } from '@/components/icons'
+import { HeaderSection } from './header-section'
 
 export function Offer() {
   return (
     <section className="bg-[#5530d6] px-4 pt-14 pb-[7.25rem]">
-      <div className="text-center pb-[2.875rem]">
-        <h2 className="text-white font-bold text-lp-sub2 leading-tight">
-          What can we offer
-        </h2>
-        <p className="text-grayscale-600 text-lp-text3 pt-5 md:max-w-[42rem] mx-auto">
-          At StarkFinder, we provide a comprehensive suite of tools and features
-          tailored for both users and developers in the Starknet ecosystem
-        </p>
-      </div>
+      <HeaderSection
+        title="What can we offer"
+        description=" At StarkFinder, we provide a comprehensive suite of tools and features
+          tailored for both users and developers in the Starknet ecosystem"
+      />
 
       <div className="grid max-md:place-items-center grid-cols-1 md:grid-cols-2 gap-4 max-w-[61.625rem] mx-auto">
         {itemOffer.map((offer, index) => (
@@ -46,7 +43,7 @@ export function Offer() {
   )
 }
 
-type SectionOfferProps = {
+export type SectionOfferProps = {
   title: string
   description: string
   src: StaticImageData
@@ -55,13 +52,15 @@ type SectionOfferProps = {
 
 function SectionOffer({ title, description, src, icon }: SectionOfferProps) {
   return (
-    <div className="bg-grayscale-900 max-w-[30.313rem] w-full rounded-2xl drop-shadow-[0_17px_17px_rgba(0,0,0,0.12)]">
-      <Image src={src} alt={title} quality={100} sizes="100vw" />
+    <div className="bg-black-scale-300 border border-[#6a49db] drop-shadow-[0_17px_17px_rgba(0,0,0,0.12)] max-w-[30.313rem] w-full overflow-hidden rounded-2xl">
+      <Image src={src} alt={title} quality={100} />
 
       <div className="flex items-start p-9 gap-5">
         <div className="pt-2">{icon}</div>
         <div>
-          <h3 className="font-bold text-2xl leading-9">{title}</h3>
+          <h3 className="font-bold text-2xl leading-9 text-grayscale-300">
+            {title}
+          </h3>
           <p className="font-medium text-lp-text2 text-grayscale-800">
             {description}
           </p>
