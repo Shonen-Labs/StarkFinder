7fadc6d14838cca37807624c9f884cc351b0501f
Luis Carrión <louiss862@gmail.com>
2025-01-31 20:36:43 -0300

    refactor: add media query for responsive

--- client/components/landing/home/header.tsx
+++ client/components/landing/home/header.tsx
@@ -4,13 +4,16 @@ export function Header() {
   return (
     <header className="sticky top-11 mx-3 z-50">
       <div className="rounded-[12.5rem] max-w-max md:max-w-[55.313rem] max-md:gap-4 mx-auto flex justify-between items-center p-3 bg-white drop-shadow-[0_4px_44px_rgba(224,227,255,0.73)]">
-        <h1 className="text-blue-navy font-bold text-[2rem]">StarkFinder</h1>
-        <nav className="flex gap-11 max-md:hidden">
+        <h1 className="text-blue-navy font-bold [@media(max-width:26.563rem)]:text-2xl text-[2rem]">
+          StarkFinder
+        </h1>
+        <nav className="flex gap-11 max-md:hidden" role="menu">
           {menuItems.map((item, index) => (
             <a
               key={`${index}-${item.name}`}
               href={item.href}
               className="text-black hover:text-orange-bright text-base font-bold transition-colors"
+              role="menuitem"
             >
               {item.name}
             </a>
