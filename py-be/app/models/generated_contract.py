32b26670a6f1464ad03145aedad98e3e6883b129
tosoham <todrsoham@gmail.com>
2025-08-29 19:09:09 +0530

    fix sorting of imports

--- py-be/app/models/generated_contract.py
+++ py-be/app/models/generated_contract.py
@@ -2,7 +2,8 @@
 
 from datetime import datetime
 
-from sqlalchemy import JSON, Column, DateTime, ForeignKey, Integer, String, Text
+from sqlalchemy import (JSON, Column, DateTime, ForeignKey, Integer, String,
+                        Text)
 
 from .base import Base
 
