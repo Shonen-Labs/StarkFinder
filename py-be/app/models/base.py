08f2b1f6c6677e27a0cc9c2bf9ec1eace7cee0a1
Odinson <bpoulav@gmail.com>
2025-08-29 10:45:19 +0530

    Update base.py

--- py-be/app/models/base.py
+++ py-be/app/models/base.py
@@ -5,7 +5,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./app.db")
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/starkfinder_test")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
